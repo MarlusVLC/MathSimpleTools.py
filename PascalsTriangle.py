28d50c46253e5fdcd7a53f66b05eaaf94d5124eb
Marlus Vinicius <marlusvinicius2014@outlook.com>
2020-08-20 10:54:12 -0300

    Mudei o PascalsTriangle de um método em P.E. para uma classe P.O.O. - Primeira interação com P.O.O em Python, para implementar diferentes interações com Triângulo de Pascal mais tarde

--- PascalsTriangle.py
+++ PascalsTriangle.py
@@ -1,14 +1,22 @@
 from math import factorial
 
-def PascalsTriangle (deg):
-    if (deg == 0):
-        return 1
-    expanBin = []
-    for i in range(deg):
-        expanBin.append(int(factorial(deg)/factorial(i)*factorial(deg-i)))
-    return tuple (expanBin)
 
-print (PascalsTriangle(2))
+class PascalsTriangle:
+
+    def graphicRep (self, deg):
+        if (deg == 0):
+            return 1
+        expanBin = []
+        for i in range(deg):
+             expanBin.append(int(factorial(deg)/factorial(i)*factorial(deg-i)))
+        return tuple (expanBin)
+
+
+pasc = PascalsTriangle()
+
+print(pasc.graphicRep(2))
+
+
 
 
 
